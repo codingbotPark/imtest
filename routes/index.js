9d46631519787d6f47e299a4abe39210078b2074
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-06-16 14:08:34 +0900

    add sources

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,13 @@ const questions=[
     questionId:1, // id는 보내지 않는다
     question:'이순신은 훈민정음을 창제하신 위인이시다',
     answer:1,
-    images:['O.jpg', 'X.jpg']
+    images:['동그라미.png', '엑스.png']
   },
   {
     questionId:2,
     question:'독도는 누구 땅일까요?',
     answer:0,
-    images:['한국.jpg', '일본.jpg']
+    images:['한국.png', '일본.png']
   },{
     questionId:3,
     question:'다음중 겉절이는 무었일까?',
@@ -24,15 +24,31 @@ const questions=[
 ]
 
 function shuffle(){
-  questions.slice();
-
+  const arr = questions.slice();
+  arr.sort(() => Math.random() - 0.5)
+  return arr
 }
 
+function readFile(fileName){
+  fs.readFile(fileName+".jpg",function(err,data){
+    
+  })
+}
 
 /* GET home page. */
 router.get('/', function(req, res) {
   const data=shuffle();
   res.json(data);
 });
+
+router.get('/image/:id',function(req,res){
+  console.log("클라이언트 요청 ㅎㅇ");
+  // console.log(req.params)
+
+  readFile('겉절이.jpg',(err,data) => {
+    if (err) {res.send()}
+    res.send(data)
+  })
+})
 
 module.exports = router;
