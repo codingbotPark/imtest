26d638b8342c586a4adc643130e115f015467a43
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-06-18 11:44:22 +0900

    add upImg

--- app.js
+++ app.js
@@ -12,9 +12,19 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// public을 정적인 파일으로 하겠다
+// app.use(express.static('public'))
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+
+// app.use('/', indexRouter);
+app.get('/',function(req,res){
+    res.send('Hello home page');
+})
+app.get('/login',function(req,res){
+    res.send("login!");
+})
 app.use('/users', usersRouter);
 
 module.exports = app;
